27b247b98d7b8275b71647c6f62ce2175ae06278
taeyoung son <ty.son@postech.ac.kr>
2025-09-08 18:39:59 +0900

    fix bug

--- trading/strategy/base.py
+++ trading/strategy/base.py
@@ -32,13 +32,11 @@ class UpperLimitStrategy(BasicStrategy):
         return int((high - low) // 4 + low)
 
     def is_buyable(self, price: float, prev_chart: pykis.KisChartBar, today_chart: pykis.KisChartBar) -> bool:
-        # make sure `yesterday` was the day that hit upper limit
-        upper_limit = float((prev_chart.close - prev_chart.open) / prev_chart.open) > 0.29
         amount_condition = float(prev_chart.amount) * 1.1 <= today_chart.amount
         open_price_condition = float(prev_chart.close) * 1.01 <= today_chart.open <= float(prev_chart.close) * 1.1
         price_condition = price >= today_chart.low
 
-        return upper_limit & amount_condition & open_price_condition & price_condition
+        return amount_condition & open_price_condition & price_condition
 
     def is_sellable(self, wallet: wallet_asset.WalletBase, price: float) -> bool:
         """returns if its sell-able"""
